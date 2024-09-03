004168d5d102e060c89c8b6e855eaaee4af55774
jackholzhauerfromthatoneschool <142598257+jackholzhauerfromthatoneschool@users.noreply.github.com>
2024-09-03 13:40:23 -0500

    wdada

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * This class supplies a utility method to reverse the entries in a linked list.
@@ -10,8 +11,19 @@ public class ListUtil
      *
      * @param strings the linked list to reverse
     */
-    public static void reverse(LinkedList<String> strings)
-    {
-        ...
+    public static void reverse(LinkedList<String> employeeNames){
+        LinkedList<String> silly = new LinkedList<String>();
+        LinkedList<String> sillys = new LinkedList<String>();
+        sillys = employeeNames;
+        employeeNames=silly;
+        silly=sillys;
+        ListIterator<String> iterator = silly.listIterator(silly.size());
+        while(iterator.hasPrevious())
+        {       employeeNames.addLast(iterator.previous());
+                iterator.remove();
+        }
+        //System.out.println(strings);
+        System.out.println(employeeNames);
+        System.out.println("Expected: [Tom, Romeo, Harry, Dick]");
     }
 }
