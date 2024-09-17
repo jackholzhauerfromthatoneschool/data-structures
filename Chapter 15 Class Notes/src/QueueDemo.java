78700d1a2e1fd11dee601190570493279e5cd0f5
jackholzhauerfromthatoneschool <142598257+jackholzhauerfromthatoneschool@users.noreply.github.com>
2024-09-17 14:16:08 -0500

    di molto

--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,21 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        //priner q of string (Linked List)
+        Queue<String> autin_priner= new LinkedList<>();
+        autin_priner.add("goobert1");
+        autin_priner.add("once more the world");
+        System.out.println("Printing:"+autin_priner.remove());
+
+        autin_priner.add("jojo");
+        autin_priner.add("fuga");
+        autin_priner.add("goobert2");
+        System.out.println("Printimg b n"+autin_priner.remove());
+
+        autin_priner.add("boss autin ded???");
+        while(autin_priner.size()>0)
+        {
+            System.out.println("Printimg b n"+autin_priner.remove());
+        }
     }
 }
--- Chapter 15 Class Notes/src/StackDemo.java
+++ Chapter 15 Class Notes/src/StackDemo.java
@@ -9,5 +9,22 @@ public class StackDemo
 {
     public static void main(String[] args)
     {
+        Stack<String>zizizliazikak = new Stack<>();
+
+        //lots of things on top
+        zizizliazikak.push("Insert: 'Austin'");
+        zizizliazikak.push("Insert: ','");
+        zizizliazikak.push("Insert: ' '");
+        zizizliazikak.push("Insert: 'za warudo'");
+        zizizliazikak.push("Insert: '?'");
+        zizizliazikak.push("Delete:'?'");
+        zizizliazikak.push("Insert:'speedwagon'");
+        //pront
+        System.out.println(zizizliazikak);
+        //ctrl z ctrl z ctrl z ctrl z
+        for(int i=0;i<4;i++)
+        {
+            System.out.println("Undo"+zizizliazikak.pop());
+        }
     }
 }
