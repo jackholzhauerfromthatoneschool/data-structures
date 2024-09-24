9c7a214722cadcfbab42da2a0ea7d905aeba5fa8
jackholz <142598257+jackholzhauerfromthatoneschool@users.noreply.github.com>
2024-09-24 00:12:30 -0500

    k,

--- Chapter 15 Activities/FloodFill/src/pair.java
+++ Chapter 15 Activities/FloodFill/src/pair.java
@@ -5,50 +5,61 @@ public class pair {
 
 public int[][] run(int s,Stack<Stack<Integer>> a, int[][] p ){
     boolean whee=true;
-    while(a.size()>0)
+    int ba=a.size();
+    Stack<Stack<Integer>> g=new Stack<Stack<Integer>>();
+    while(ba>0)
     {
-        Stack<Integer> z = a.pop();
-        int row=z.pop();
-        int col=z.pop();       
+        Stack<Integer> za = a.pop();
+        int col=za.pop();
+        int row=za.pop();
+        if (p[row][col]==0){       
         p[row][col]=s;
+        s+=1;
+        }
         if(row!=9)
         if(p[row+1][col]==0)
         {
+            Stack<Integer> z=new Stack<Integer>();
             z.push(row+1);
             z.push(col);
-            a.push(z);
+            g.push(z);
             whee=false;
         }
         if(row!=0)
         if(p[row-1][col]==0)
         {
+            Stack<Integer> z=new Stack<Integer>();
             z.push(row-1);
             z.push(col);
-            a.push(z);
+            g.push(z);
             whee=false;
         }
         if(col!=9)
         if(p[row][col+1]==0)
         {
+            Stack<Integer> z=new Stack<Integer>();
             z.push(row);
             z.push(col+1);
-            a.push(z);
+            g.push(z);
             whee=false;
         }
         if(col!=0)
         if(p[row][col-1]==0)
         {
+            Stack<Integer> z=new Stack<Integer>();
             z.push(row);
             z.push(col-1);
-            a.push(z);
+            g.push(z);
             whee=false;
         }
+        //System.out.println(a);
+        ba-=1;
         
     }
     if (s>=99||whee)
     return p;
     else{
-        return run(s+1,a,p);
+        return run(s,g,p);
     }
 }
 }
